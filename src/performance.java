7e1a6f094db6afb06dd5a559772cdabcb4758f1d
mxteuss <ovagabundant@gmail.com>
2025-06-05 11:51:43 -0300

    default

---
+++ src/performance.java
@@ -0,0 +1,39 @@
+public class performance {
+
+        public static void main(String[] args) {
+            long start = System.currentTimeMillis();
+            concatString(30_000);
+            long end = System.currentTimeMillis();
+            System.out.println("Tempo gasto para String  " + (end - start) + "ms");
+            start = System.currentTimeMillis();
+            concatStringBuilder(1000_000);
+            end = System.currentTimeMillis();
+            System.out.println("Tempo gasto para String  " + (end - start) + "ms");
+
+            start = System.currentTimeMillis();
+            concatStringBuffer(1000_000);
+            end = System.currentTimeMillis();
+            System.out.println("Tempo gasto para String  " + (end - start) + "ms");
+        }
+
+
+        private static void concatString(int tamanho) {
+            String frase = " ";
+            for (int i = 0; i < tamanho; i++) {
+                frase += i;
+            }
+
+        }
+        private static void concatStringBuilder(int tamanho) {
+            StringBuilder sb = new StringBuilder(tamanho);
+            for (int i = 0; i < tamanho; i++) {
+                sb.append(i);
+            }
+        }
+        private static void concatStringBuffer(int tamanho) {
+            StringBuffer sb = new StringBuffer(tamanho);
+            for (int i = 0; i < tamanho; i++) {
+                sb.append(i);
+            }
+        }}
+
